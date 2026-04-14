d24d21bec748be387ff899cd6bd5bbdc6c8d1b2d
Amarachi Ogueji <amarachiogueji@gmail.com>
2026-04-14 05:14:55 +0100

    Initial project setup

---
+++ data/cart.js
@@ -0,0 +1,13 @@
+/* How do we know which product to add:
+To achieve this, we will use a feature of html called DATA ATTRIBUTE (DA).
+DA is just another HTML attribute.
+DA allows us to attach any information to an element
+
+
+
+
+*/
+
+
+const cart = [];
+
--- script/amazon.js
+++ script/amazon.js
@@ -12,7 +12,7 @@ Data means information. In our case  with the amazon product, it means saving th
 
  */
 
-const product = [{
+/*const product = [{
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
 
@@ -40,7 +40,18 @@ rating: {
 },
 priceCents: 799
 
-}];
+}, {
+image: 'images/products/black-2-slot-toaster.jpg',
+name: '2 Slot Toaster - Black',
+
+rating: {
+    stars: 5,
+    count: 2197,    
+},
+priceCents: 1899
+
+}
+];*/
 
 //all the above object in the variable is called a data structure because it organzes the data. Also, note that this data consist of a list of product 
 
@@ -50,28 +61,28 @@ priceCents: 799
 
 let productsHTML = '';
 
-product.forEach((product) => {
+products.forEach((products) => {
    productsHTML +=  `
        <div class="product-container">
                 <div class="product-image-container">
                     <img class="product-image"
-                    src="${product.image}">
+                    src="${products.image}">
                 </div>
 
                 <div class="product-name limit-text-to-2-lines">
-                    ${product.name}
+                    ${products.name}
                 </div>
 
                 <div class="product-rating-container">
                     <img class="product-rating-stars"
-                    src="images/ratings/rating-${product.rating.stars * 10}.png">
+                    src="images/ratings/rating-${products.rating.stars * 10}.png">
                     <div class="product-rating-count link-primary">
-                    ${product.rating.count}
+                    ${products.rating.count}
                     </div>
                 </div>
 
                 <div class="product-price">
-                    $${(product.priceCents / 100).toFixed(2)}
+                    $${(products.priceCents / 100).toFixed(2)}
                 </div>
 
                 <div class="product-quantity-container">
@@ -96,15 +107,33 @@ product.forEach((product) => {
                     Added
                 </div>
 
-                <button class="add-to-cart-button button-primary">
+                <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${products.id}">
                     Add to Cart
                 </button>
                 </div>
+
+                 
   `;
 
 });
 
-  console.log(productsHTML);
+/* How do we know which product to add:
+To achieve this, we will use a feature of html called DATA ATTRIBUTE (DA).
+DA is just another HTML attribute.
+DA allows us to attach any information to an element
+SYNTAX RULE FOR DATA ATTRIBUTE:
+DA is just an HTML attribute. It has a name and a value. eg: below:
+
+data-product-name="${products.name}
+data-product-name= (name). DA must strate with "data-" and then we can give it any name we want, in our case we used "product-name". We must also seperate the words with "-"
+
+"${products.name}(value)
+
+NB: we can also attach product image and product price to the data attribute. 
+
+
+
+*/
 
 
   //random fact: the .toFixed() method is used to show a number with two decimal places in javascript 
@@ -113,3 +142,60 @@ product.forEach((product) => {
 //Finally, we put the product on the page by using the html to do so. We do so below:
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+
+/*
+The benefits of generating the html in js includes is
+if we want to add another product all we need to go up to the data and add a new product. 
+
+ */
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+   button.addEventListener('click', () => {
+
+   const productId = button.dataset.productId;
+
+
+
+let matchingItem;
+
+   cart.forEach((item) => {
+    if (productId === item.productId) {
+        matchingItem = item;
+    }
+   });
+
+if (matchingItem) {
+    matchingItem.quantity += 1;
+} else {
+    cart.push({
+    productId: productId, 
+    quantity: 1
+   });
+}
+
+
+
+   console.log(cart);
+   });
+
+});
+
+/*
+How to add multiple quantities to cart. Step by step:
+1. first we will chck if product is already in cart
+2. if it is in cart, increate the quantity by 1
+3. if it is not in the cart, add it to cart. 
+
+*/
+
+
+/*
+Problem:
+
+What if we have 2 products that have the same name but they are from different sellers, how do we differenciate them: 
+To flx thi: 
+
+We will give each product an id
+*/
